4098d9a57a910aebdba3d7bc65d0ddcaf7038180
RVFSWD <146232937+RVFSWD@users.noreply.github.com>
2023-12-09 06:59:17 +0800

    Update script.js - added functions in order design section

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// order section ####################### 
+
+function changeImage(imageSrc, eventText) {
+  document.getElementById('carImage').src = './Models images/' + imageSrc;
+  document.getElementById('event1').innerHTML = eventText;
+}
+
+function changeToNewImage() {
+
+  var imageElement = document.getElementById('carImage');
+
+
+  imageElement.src = 'tms_color_blue_01_v2.png';
+  imageElement.src = 'tms_color_gray_01_v2.png';
+}
+
+
+
 // emialer #########################
 
 
@@ -20,4 +38,8 @@
         alert('Please enter a valid email address.');
       }
     }
+  
+
+
+
 
